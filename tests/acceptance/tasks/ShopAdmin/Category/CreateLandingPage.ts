95d8385fe8aa3cd286f67688360a45f0270d9d59
vanpham-sw <d.vanpham@shopware.com>
2024-10-23 17:49:33 +0700

    NEXT-38339 - Categories - As an admin user, I would like to add a landing page.

---
+++ tests/acceptance/tasks/ShopAdmin/Category/CreateLandingPage.ts
@@ -0,0 +1,47 @@
+import { test as base } from '@playwright/test';
+import type { FixtureTypes, Task } from '@fixtures/AcceptanceTest';
+
+export const CreateLandingPage = base.extend<{ CreateLandingPage: Task }, FixtureTypes>({
+    CreateLandingPage: async ({ ShopAdmin, AdminCategories, AdminLandingPageCreate, AdminLandingPageDetail, TestDataService }, use ) => {
+
+        const task = (layoutName: string, landingPageData, status: boolean) => {
+            return async function CreateLandingPage() {
+                await AdminCategories.landingPageHeadline.click();
+                await AdminCategories.addLandingPageButton.click();
+
+                await ShopAdmin.expects(AdminLandingPageDetail.saveLandingPageButton).toBeVisible();
+                await ShopAdmin.expects(AdminLandingPageDetail.saveLandingPageButton).toContainText('Save');
+
+                //Fill details and save
+                await AdminLandingPageCreate.nameInput.fill(landingPageData.name);
+                await AdminLandingPageCreate.landingPageStatus.setChecked(landingPageData.status);
+                await AdminLandingPageCreate.salesChannelSelectionList.click();
+                await AdminLandingPageCreate.filtersResultPopoverItemList.filter({ hasText: landingPageData.salesChannel }).click();
+                await AdminLandingPageCreate.seoUrlInput.fill(landingPageData.seoUrl);
+
+                if (layoutName) {
+                    await AdminLandingPageCreate.layoutTab.click();
+                    // Verify empty layout state
+                    await ShopAdmin.expects(AdminLandingPageCreate.layoutEmptyState).toBeVisible();
+                    await ShopAdmin.expects(AdminLandingPageCreate.createNewLayoutButton).toBeVisible();
+                    // Select existing layout
+                    await AdminLandingPageCreate.assignLayoutButton.click();
+                    await AdminLandingPageCreate.loadingSpinner.waitFor({ state: 'hidden' });
+                    await AdminLandingPageCreate.searchLayoutInput.dblclick();
+                    // Search input need to delay press more than 300ms to mimic user typing in order to activate search action
+                    await AdminLandingPageCreate.searchLayoutInput.pressSequentially(layoutName.split(' ')[1].substring(0,5), {delay: 500});
+                    await AdminLandingPageCreate.layoutItems.first().waitFor({ state: 'visible' });
+                    await AdminLandingPageCreate.layoutItems.first().click();
+                    await AdminLandingPageCreate.layoutSaveButton.click();
+                }
+                await AdminLandingPageCreate.saveLandingPageButton.click();
+                await AdminLandingPageCreate.loadingSpinner.waitFor({ state: 'hidden' });
+                const url = AdminLandingPageDetail.page.url();
+                const landingPageId = url.split('/')[url.split('/').length - 2];
+                TestDataService.addCreatedRecord('landing_page', landingPageId);
+            }
+        }
+
+        await use(task);
+    },
+});
